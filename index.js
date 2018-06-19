d925cc54e5f8fa90afb17c2989ab7d245b174485
fabi <fabienne.gerschwiler@gmail.com>
2018-06-19 19:22:41 +0200

    api call

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var messages;
+var url = "https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/fb1ba974-ac5c-40f2-8236-faf5a657df49?subscription-key=3bfef3d064f24ccb8eaa4edab7305499&verbose=true&timezoneOffset=0&q";
 
 
 function loadData() {
@@ -35,6 +35,9 @@ function sendQuestion() {
 $(document).ready(function(){
     $("#sendButton").click(function(){
         console.log("Sennd Question");
-        console.log($("#question").text);
+        var fullurl = this.url + $("#question").val();
+        $.get(fullurl, function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
     });
 });
