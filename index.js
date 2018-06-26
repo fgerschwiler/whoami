d54925baa7008b96b84049a9b668ff1e47cc7b1f
MichaelBaehren <michi-b@gmx.ch>
2018-06-26 20:27:01 +0200

    erweiterung Abfrage Entities von LUIS
    Entities von LUIS abfragen und für Relation Configuration aufbereiten

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var url = "https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/fb1ba974-ac5c-40f2-8236-faf5a657df49?subscription-key=3bfef3d064f24ccb8eaa4edab7305499&verbose=true&timezoneOffset=0&q=";
+var apiKey = ""
+
 
 
 function loadData() {
@@ -48,3 +50,57 @@ $(document).ready(function(){
         });
     });
 });
+
+var subscriptionKey = "14f36810bcf046209f79ba69a13deabe";
+var applicationKey = "fb1ba974-ac5c-40f2-8236-faf5a657df49";
+var baseUrl = "https://westeurope.api.cognitive.microsoft.com/luis/";
+var apiUrl = baseUrl + "api/v2.0/apps/";
+var appVersion = "0.2";
+
+$(document).ready(getAllEntitiesFromLuis);
+
+function getAllEntitiesFromLuis() {
+var entitiesUrl = apiUrl + applicationKey + "/versions/" + appVersion + "/entities/";
+
+$.ajax({
+        url: entitiesUrl,
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+        type: "GET",
+        })
+        .done(function(data) {
+            console.log("Intent: " + JSON.stringify(data));
+            var entities = data[1];
+
+           for (entity of data)
+            {
+                $("#response").append($('<p>' + entity.name + '</p>'));
+                $("#response").append($('<ul>'));
+                for (role of entity.roles){
+                    $("#response").append($('<li>' + role.name + '</li>'));
+                    if (entity.name == "Tier"){
+                        $("#animals").append($('<option value="' + role.name + '">'  + role.name +  '</option>'));
+                    }
+                } 
+                $("#response").append($('</ul>'));
+            }
+        })
+        .fail(function() {
+            console.error("error");
+        });    
+};
+
+function CeckedEntityInsertIntoDatabase(id, name){
+    if (getEntityFromDatabaseWithId(id) == null){
+        insertEntityIntoDatabase(id,name);
+    }
+}
+
+function getEntityFromDatabaseWithId(id){
+    return false;
+}
+
+function updateEntityIntoDatabase(id, name){
+    //mit PUT update (wenn nicht vorhanden wir automatisch eingefügt)
+}
